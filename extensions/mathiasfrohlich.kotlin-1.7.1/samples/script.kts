b8eed23bb72824128ece1d9ae1d1a28b668c7281
Demys Cota <demys.cota@snowmanlabs.com>
2021-01-11 17:26:15 -0300

    jest snippets

--- extensions/mathiasfrohlich.kotlin-1.7.1/samples/script.kts
+++
@@ -1,4 +0,0 @@
-import java.io.File
-
-val folders = File(args[0]).listFiles { file -> file.isDirectory() }
-folders?.forEach { folder -> println(folder) }
--- extensions/richardwillis.vscode-gradle-3.5.2/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Change Log
-
-Detailed release notes can be found here: https://github.com/badsyntax/vscode-gradle/releases
