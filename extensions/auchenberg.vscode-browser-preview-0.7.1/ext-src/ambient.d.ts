5c241ab6f256cf220866768744fd99e665e9b9af
Demys Cota <demys.cota@snowmanlabs.com>
2021-04-27 18:34:42 -0300

    remove extesions folder

--- extensions/auchenberg.vscode-browser-preview-0.7.1/ext-src/ambient.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'karma-chrome-launcher' {
-  type OSMap = Record<string, string | null>;
-  export const example: ['type', { prototype: { DEFAULT_CMD: OSMap } }];
-}
-
-declare module '@chiragrupani/karma-chromium-edge-launcher' {
-  type OSMap = Record<string, string | null>;
-  export const example: ['type', { prototype: { DEFAULT_CMD: OSMap } }];
-}
